7492cbe5f3aa46a00bfce774129e47ebae6f97cb
BrigmannMichael <150007692+BrigmannMichael@users.noreply.github.com>
2023-11-10 12:11:13 +0800

    Add files via upload

--- WiFi Server Simple/src/main.cpp
+++ WiFi Server Simple/src/main.cpp
@@ -6,16 +6,20 @@
 const int buttonPin = 25;
 const int servoPin = 18;
 Servo servo;
-int counter = 0;
+int pos = 0;
 const char* ssid     = "brigmann";
 const char* password = "Anm3ldun9";
+
+String header;
+const int LEDPIN = 5;
+String LEDSTATE = "off";
 
 WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(5, OUTPUT);      // set the LED pin mode
+    pinMode(LEDPIN, OUTPUT);      // set the LED pin mode
     servo.attach(servoPin);
     pinMode(buttonPin, INPUT);
 
@@ -52,8 +56,7 @@ void loop(){
     Serial.println("New Client.");           // print a message out the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
     while (client.connected()) {            // loop while the client's connected
-      if (client.available()) 
-      {             // if there's bytes to read from the client,
+      if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
         if (c == '\n') {                    // if the byte is a newline character
@@ -68,10 +71,45 @@ void loop(){
             client.println();
 
             // the content of the HTTP response follows the header:
+            
+
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+
+            client.println("<style>html { font-family: monospace; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: yellowgreen; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 32px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: gray;}</style></head>");
+
+            client.println("<body><h1>Servo Motor Control</h1>");
+            client.println("<p>Brigmann Engineering</p>");
+
             client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
             client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
-            client.print("Servo <a href=\"/S\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 45 <a href=\"/A\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 90 <a href=\"/B\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 135 <a href=\"/C\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 315 <a href=\"/A\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 270 <a href=\"/B\">here</a> to activate the servo motor.<br>");
+            client.print("Servo 225 <a href=\"/C\">here</a> to activate the servo motor.<br>");
 
+            
+            if (LEDSTATE == "off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+
+            if (header.indexOf("GET /5/on") >= 0) {
+              LEDSTATE = "on";
+              digitalWrite(LEDPIN, HIGH);               // turns the LED on
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              LEDSTATE = "off";
+              digitalWrite(LEDPIN, LOW);  }
+
+          
             // The HTTP response ends with another blank line:
             client.println();
             // break out of the while loop:
@@ -87,36 +125,75 @@ void loop(){
         if (currentLine.endsWith("GET /H")) {
           digitalWrite(5, HIGH);               // GET /H turns the LED on
         }
+        if (currentLine.endsWith("GET /5/on")) {
+          digitalWrite(5, HIGH);               // GET /H turns the LED on
+        }
         if (currentLine.endsWith("GET /L")) {
           digitalWrite(5, LOW);                // GET /L turns the LED off
         }
-        if (currentLine.endsWith("GET /S")) 
-        {
+        if (currentLine.endsWith("GET /A")) {
           digitalWrite(25, LOW);   
-          if (buttonState == LOW)
-          {
-            counter++;
-            delay(150);
-  
-
-            if(counter == 0)
-                servo.write (20);  // zero degrees
-            else if(counter == 1)
-                servo.write(90);
-            else if(counter == 2)
-                servo.write (150); 
-            else if(counter == 3)  
-                servo.write (180);
-                                                      //else reset the counter to 0 which resets the servo to 0 degrees  
-            else
-            counter = 0;
-            }
-        }
-      }
-    }
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 45; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 45; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+   if (currentLine.endsWith("GET /B")) {
+          digitalWrite(25, LOW);   
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 90; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 90; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+   if (currentLine.endsWith("GET /C")) {
+          digitalWrite(25, LOW);   
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 135; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 135; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+  if (currentLine.endsWith("GET /D")) {
+          digitalWrite(25, LOW);   
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 315; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 315; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+   if (currentLine.endsWith("GET /E")) {
+          digitalWrite(25, LOW);   
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 270; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 270; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+   if (currentLine.endsWith("GET /F")){ 
+          digitalWrite(25, LOW);   
+          if (buttonState == LOW){
+          	for (pos = 0; pos <= 225; pos += 1) { 
+		      		servo.write(pos);   
+            		delay(15); }
+          	for (pos = 225; pos >= 0; pos -= 1) { 
+	          	servo.write(pos);    
+	            	delay(15);             
+	}}}
+      }}
       
          // close the connection:
     client.stop();
     Serial.println("Client Disconnected.");
-    }
-}
+      }}
